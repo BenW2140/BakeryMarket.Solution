390558f339488446bf5323b5146d8254383936ef
Ben White <bwhite2140@outlook.com>
2020-08-14 10:38:28 -0700

    add login and register view models

---
+++ BakeryMarket/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BakeryMarket.ViewModels
+{
+  public class RegisterViewModels
+  {
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+  }
+}
